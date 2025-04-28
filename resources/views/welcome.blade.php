e1ac3b74cb0cb1fa2f8e23caefd58db610e2f8e9
mohaimenkhalid <mohaimen707@gmail.com>
2025-04-29 00:55:08 +0600

    design blog card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,48 +11,77 @@
                 <div class="">
                     <div class="card sidebar-category">
                         <h5>Categories</h5>
+                        <ul>
+                            <li>Sedan car(2)</li>
+                            <li>Bus(7)</li>
+                            <li>Sedan car(0)</li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <div class="col-9">
                <div class="row">
                    <div class="col-4">
-                       <div class="card" style="width: 100%;">
+                       <div class="card product-card" style="width: 100%;">
                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
                            <div class="card-body">
-                               <h5 class="card-title">Card title</h5>
-                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                               <div>
+                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                               </div>
+
+                               <h5 class="mt-3">Top car finding website</h5>
+                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                               <a href="#" class="">Read More</a>
                            </div>
                        </div>
                    </div>
                    <div class="col-4">
-                       <div class="card" style="width: 100%;">
+                       <div class="card product-card" style="width: 100%;">
                            <img src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?semt=ais_hybrid&w=740" class="card-img-top" alt="...">
                            <div class="card-body">
-                               <h5 class="card-title">Card title</h5>
-                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                               <div>
+                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                               </div>
+
+                               <h5 class="mt-3">Top car finding website</h5>
+                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                               <a href="#" class="">Read More</a>
                            </div>
                        </div>
                    </div>
                    <div class="col-4">
-                       <div class="card" style="width: 100%;">
+                       <div class="card product-card" style="width: 100%;">
                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
                            <div class="card-body">
-                               <h5 class="card-title">Card title</h5>
-                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                               <div>
+                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                               </div>
+
+                               <h5 class="mt-3">Top car finding website</h5>
+                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                               <a href="#" class="">Read More</a>
                            </div>
                        </div>
                    </div>
                    <div class="col-4">
-                       <div class="card" style="width: 100%;">
+                       <div class="card product-card" style="width: 100%;">
                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
                            <div class="card-body">
-                               <h5 class="card-title">Card title</h5>
-                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                               <div>
+                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                               </div>
+
+                               <h5 class="mt-3">Top car finding website</h5>
+                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                               <a href="#" class="">Read More</a>
                            </div>
                        </div>
                    </div>
