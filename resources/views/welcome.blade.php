c5e9b5f5365dc91a2a09a0da547702290e5587d7
mohaimenjatri <97215006+mohaimenjatri@users.noreply.github.com>
2025-04-28 17:45:22 +0600

    initial design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,67 @@
 @extends('layouts.app')
 @section('content')
-<h1>Main body section</h1>
+    <div class="top-banner">
+        <h1 class="top-banner-header">Top Banner Section</h1>
+    </div>
+
+
+    <div class="container d-flex">
+        <div class="row mt-3">
+            <div class="col-3">
+                <div class="">
+                    <div class="card sidebar-category">
+                        <h5>Categories</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+               <div class="row">
+                   <div class="col-4">
+                       <div class="card" style="width: 100%;">
+                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                           <div class="card-body">
+                               <h5 class="card-title">Card title</h5>
+                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-4">
+                       <div class="card" style="width: 100%;">
+                           <img src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?semt=ais_hybrid&w=740" class="card-img-top" alt="...">
+                           <div class="card-body">
+                               <h5 class="card-title">Card title</h5>
+                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-4">
+                       <div class="card" style="width: 100%;">
+                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                           <div class="card-body">
+                               <h5 class="card-title">Card title</h5>
+                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-4">
+                       <div class="card" style="width: 100%;">
+                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                           <div class="card-body">
+                               <h5 class="card-title">Card title</h5>
+                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                               <a href="#" class="btn btn-primary">Go somewhere</a>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+            </div>
+
+        </div>
+
+    </div>
 @endsection
 
 
