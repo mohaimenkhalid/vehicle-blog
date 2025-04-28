b9811e86ae23d26dede4fb1f9df7e4b113d0d774
mohaimenkhalid <mohaimen707@gmail.com>
2025-04-29 01:32:03 +0600

    sidebar update

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,14 +1,17 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Car Blog</a>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <i class="fa-solid fa-taxi"></i>
+                Car Blog
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Popular Car</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,101 @@
     </div>
 
 
-    <div class="container d-flex">
+    <div class="container">
         <div class="row mt-3">
-            <div class="col-3">
-                <div class="">
-                    <div class="card sidebar-category">
-                        <h5>Categories</h5>
-                        <ul>
-                            <li>Sedan car(2)</li>
-                            <li>Bus(7)</li>
-                            <li>Sedan car(0)</li>
-                        </ul>
+            <div class="col-9">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card product-card" style="width: 100%;">
+                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <div>
+                                    <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                    <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                    <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                                </div>
+
+                                <h5 class="mt-3">Top car finding website</h5>
+                                <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                                <a href="#" class="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card product-card" style="width: 100%;">
+                            <img src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?semt=ais_hybrid&w=740" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <div>
+                                    <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                    <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                    <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                                </div>
+
+                                <h5 class="mt-3">Top car finding website</h5>
+                                <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                                <a href="#" class="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card product-card" style="width: 100%;">
+                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <div>
+                                    <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                    <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                    <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                                </div>
+
+                                <h5 class="mt-3">Top car finding website</h5>
+                                <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                                <a href="#" class="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card product-card" style="width: 100%;">
+                            <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <div>
+                                    <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
+                                    <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
+                                    <i class="fa-solid fa-eye mx-1"></i><span>10</span>
+                                </div>
+
+                                <h5 class="mt-3">Top car finding website</h5>
+                                <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                                <a href="#" class="">Read More</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-9">
-               <div class="row">
-                   <div class="col-4">
-                       <div class="card product-card" style="width: 100%;">
-                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <div>
-                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
-                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
-                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
-                               </div>
-
-                               <h5 class="mt-3">Top car finding website</h5>
-                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                               <a href="#" class="">Read More</a>
-                           </div>
-                       </div>
-                   </div>
-                   <div class="col-4">
-                       <div class="card product-card" style="width: 100%;">
-                           <img src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?semt=ais_hybrid&w=740" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <div>
-                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
-                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
-                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
-                               </div>
-
-                               <h5 class="mt-3">Top car finding website</h5>
-                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                               <a href="#" class="">Read More</a>
-                           </div>
-                       </div>
-                   </div>
-                   <div class="col-4">
-                       <div class="card product-card" style="width: 100%;">
-                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <div>
-                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
-                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
-                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
-                               </div>
-
-                               <h5 class="mt-3">Top car finding website</h5>
-                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                               <a href="#" class="">Read More</a>
-                           </div>
-                       </div>
-                   </div>
-                   <div class="col-4">
-                       <div class="card product-card" style="width: 100%;">
-                           <img src="https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1600,c_limit/DSC_5903.jpg" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <div>
-                                   <i class="fa-solid fa-tag mx-1"></i><span>user car</span>
-                                   <i class="fa-solid fa-calendar-days mx-1"></i><span>20 Dec, 2020</span>
-                                   <i class="fa-solid fa-eye mx-1"></i><span>10</span>
-                               </div>
-
-                               <h5 class="mt-3">Top car finding website</h5>
-                               <p class="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
-                               <a href="#" class="">Read More</a>
-                           </div>
-                       </div>
-                   </div>
-               </div>
+            <div class="col-3">
+                <div class="card sidebar-category">
+                    <h5>Categories</h5>
+                    <ul>
+                        <li>Sedan car(2)</li>
+                        <li>Bus(7)</li>
+                        <li>Sedan car(0)</li>
+                    </ul>
+                </div>
+                <div class="card sidebar-category my-3">
+                    <h5>Latest blogs</h5>
+                    <ul>
+                        <li>
+                            <a href="#">How to Buy a new car</a>
+                        </li>
+                        <li>
+                            <a href="#">How to Buy a new car</a>
+                        </li>
+                        <li>
+                            <a href="#">How to Buy a new car</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
         </div>
-
     </div>
 @endsection
 
